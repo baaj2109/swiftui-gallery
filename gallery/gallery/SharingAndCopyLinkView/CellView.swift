cd4d925c6bfb537e31dc96b6ede2ee1f67d1c6b2
kehwaweng <kehwaweng@staff.ipevo.com>
2024-02-01 09:39:23 +0800

    add share view

--- gallery/gallery/SharingAndCopyLinkView/CellView.swift
+++ gallery/gallery/SharingAndCopyLinkView/CellView.swift
@@ -8,8 +8,97 @@
 import SwiftUI
 
 struct CellView: View {
+    @State var show = false
+    @State var lineWidth : CGFloat = 4
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+//            Circle()
+//                .frame(width: 250, height: 250)
+//                .opacity(0.1)
+            VStack {
+                HStack{
+                    Image(systemName: "car.fill")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 60, height:  60)
+                        .overlay {
+                            Circle()
+                                .stroke(lineWidth: lineWidth)
+                        }
+                        .offset(x: show ? 115: 300, y:show ?  -10: -100)
+//                        .offset(x:115, y: -10)
+                    Image(systemName: "car.fill")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 60, height:  60)
+                        .overlay {
+                            Circle()
+                                .stroke(lineWidth: lineWidth)
+                        }
+                        .offset(x: show ? -115: -300, y:show ?  -10: -100)
+//                        .offset(x:-115, y: -10)
+                }
+                HStack{
+                    Image(systemName: "car.fill")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 60, height:  60)
+                        .overlay {
+                            Circle()
+                                .stroke(lineWidth: lineWidth)
+                        }
+                        .offset(x: show ? 115: 300, y:show ?  10: 100)
+//                        .offset(x: 115, y: 10)
+                    Image(systemName: "car.fill")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 60, height:  60)
+                        .overlay {
+                            Circle()
+                                .stroke(lineWidth: lineWidth)
+                        }
+                        .offset(x: show ? -115: -300, y:show ?  10: 100)
+//                        .offset(x: -115, y: 10)
+                }
+            }
+            VStack{
+                Image(systemName: "car.fill")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 60, height:  60)
+                    .overlay {
+                        Circle()
+                            .stroke(lineWidth: lineWidth)
+                    }
+                    .offset(y:show ?  -60: -300)
+//                    .offset( y: -60)
+                Image(systemName: "car.fill")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 60, height:  60)
+                    .overlay {
+                        Circle()
+                            .stroke(lineWidth: lineWidth)
+                    }
+                    .offset(y:show ?  60: 300)
+//                    .offset( y: 60)
+            }
+            Image(systemName: "car.fill")
+                .resizable()
+                .scaledToFill()
+                .frame(width: 60, height:  60)
+                .overlay {
+                    Circle()
+                        .stroke(lineWidth: lineWidth)
+                }
+        }
+        .rotationEffect(.degrees(show ? 0 : 100))
+        .onAppear(){
+            withAnimation(.spring(response: 0.8, dampingFraction: 0.6)) {
+                show.toggle()
+            }
+        }
+        .padding()
     }
 }
 
--- gallery/gallery/SharingAndCopyLinkView/ShareView.swift
+++ gallery/gallery/SharingAndCopyLinkView/ShareView.swift
@@ -8,11 +8,91 @@
 import SwiftUI
 
 struct ShareView: View {
+    @Binding var showShareView: Bool
+    @State var copylink = false
+    var link = "http://www.tmp.com"
+    var link2 = "http://www.tmp2.com"
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Button {
+                //
+            } label: {
+                Image(systemName: "arrow.left")
+                    .font(.title2)
+                    .background(.gray.opacity(0.15), in: Circle())
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            .tint(.black)
+            Spacer()
+            Group{
+                Text("Invite your friend\nto discover the app")
+                    .bold()
+                    .font(.largeTitle)
+                Text("Your moments stay yours alone. opt to share with family. ganing insight into their lives as well-sharing is optional.")
+                    .frame(width: 300)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            Spacer()
+            CellView()
+                .frame(width: 300)
+            Spacer()
+            HStack(spacing: 14) {
+                Button {
+                    //
+                    UIPasteboard.general.string = link
+                   
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                        withAnimation {
+                            copylink.toggle()
+                        }
+                    }
+                } label: {
+                    VStack{
+                        Image(systemName: "doc.on.doc")
+                            .font(.title)
+                        Text("Copy link")
+                            .font(.custom("BradleyHandITCTT-Bold", size: 20))
+                    }
+                    .frame(width: 160, height: 140)
+                    .background(.gray.opacity(0.15), in: RoundedRectangle(cornerRadius: 30))
+                    
+                }
+                ShareLink(item: (URL(string: link) ?? URL(string: link2))!) {
+                    VStack{
+                        Image(systemName: "doc.on.doc")
+                            .font(.title)
+                        Text("Copy link")
+                            .font(.custom("BradleyHandITCTT-Bold", size: 20))
+                    }
+                    .frame(width: 160, height: 140)
+                    .background(.gray.opacity(0.15), in: RoundedRectangle(cornerRadius: 30))
+                }
+            }
+            .tint(.primary)
+            Spacer()
+            Button {
+                //
+            } label: {
+                Text("Maybe later")
+                    .font(.system(size: 25))
+                    .opacity(0.5)
+            }
+            .tint(.primary)
+
+        }
+        .overlay(alignment: .bottom) {
+            Text("Link Copied")
+                .bold()
+                .font(.headline)
+                .frame(minWidth: 150, minHeight: 50)
+                .background(.white, in: RoundedRectangle(cornerRadius: 30))
+                .shadow(color: .black.opacity(0.1), radius: 10, x: 0, y:0)
+                .offset(y: copylink ? 0 : 100)
+        }
+        .padding(.horizontal, 30)
     }
 }
 
 #Preview {
-    ShareView()
+    ShareView(showShareView: .constant(false))
 }
