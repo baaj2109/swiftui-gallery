b7c315c696897d7b3666b413176dbb2bad21c609
kehwaweng <kehwaweng@staff.ipevo.com>
2024-02-01 09:38:31 +0800

    add share view

---
+++ gallery/gallery/SharingAndCopyLinkView/CellView.swift
@@ -0,0 +1,18 @@
+//
+//  CellView.swift
+//  gallery
+//
+//  Created by kehwa weng on 2024/2/1.
+//
+
+import SwiftUI
+
+struct CellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CellView()
+}
---
+++ gallery/gallery/SharingAndCopyLinkView/ShareView.swift
@@ -0,0 +1,18 @@
+//
+//  ShareView.swift
+//  gallery
+//
+//  Created by kehwa weng on 2024/2/1.
+//
+
+import SwiftUI
+
+struct ShareView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShareView()
+}
